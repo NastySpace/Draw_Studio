3581f5b6d8eef183eb77d2dee1cc2073b46286fd
HoloDD <nikvk2001@gmail.com>
2022-05-04 15:02:37 +0300

    first screen component todo

---
+++ client/src/components/first-screen/FirstScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './FirstScreen.module.css';
+import logo from '../../assets/logo.svg';
+
+const FirstScreen = () => {
+    return <></>;
+};
+
+export default FirstScreen;
