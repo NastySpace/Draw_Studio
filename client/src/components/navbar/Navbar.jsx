7cc2db2c972fdabff4ae49bbf94cbe254c64aff3
NastySpace <anastasiia.telizhenko@gmail.com>
2022-05-11 13:33:09 +0300

    add icon

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from './Navbar.module.css';
-import logo from '../../assets/logo.svg';
-
+import logo from '../../assets/img/logo.svg';
+import homeIcon from '../../assets/img/nav-home.svg';
+import homeIcon from '../../assets/img/order-home.svg';
+import homeIcon from '../../assets/img/nav-home.svg';
 const Navbar = () => {
     return (
         <div className={styles.navbar}>
             <img src={logo} className={styles.logo}></img>
             <nav className={styles.nav}>
                 <div href="" className={styles.nav_item}>
-                    <img className={styles.icon}></img>
+                    <img src={homeIcon} className={styles.icon}></img>
                     <div className={styles.label}>Головна</div>
                 </div>
                 <div href="" className={styles.nav_item}>
