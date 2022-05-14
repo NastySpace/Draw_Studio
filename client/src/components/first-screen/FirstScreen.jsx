bdc11ac5e1d1ef67d062fe4dd2917d2c0b0bf5a8
NastySpace <anastasiia.telizhenko@gmail.com>
2022-05-14 22:55:02 +0300

    added photo, text, styles

--- client/src/components/first-screen/FirstScreen.jsx
+++ client/src/components/first-screen/FirstScreen.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import styles from './FirstScreen.module.css';
-
+import onePhoto from '../../assets/img/three.jpg';
+import twoPhoto from '../../assets/img/black.jpg';
 const FirstScreen = () => {
     return (
         <div className={styles.screen}>
-            <div>
-                <h2></h2>
-                <h1></h1>
-                <div></div>
-                <p></p>
-                <button></button>
+
+            <div className={styles.text}>
+
+                <h2 className={styles.h2}>Trending 2022</h2>
+                <h1 className={styles.h1}>Draw Studio</h1>
+                <p className={styles.p}>Одяг який зробить ваший образ неймовірним.Ви можете замовити готовий малюнок або обрати свій! </p>
+                <button className={styles.shop}>Shop now</button>
             </div>
             <div>
                 <div>
-                    <img src="" alt="" />
+                    <img className={styles.onePhoto} src={onePhoto} alt="" />
                 </div>
                 <div>
-                    <img src="" alt="" />
+                    <img className={styles.twoPhoto} src={twoPhoto} alt="" />
                 </div>
                 <div>
                     <img src="" alt="" />
